3c3e0e5da1994b3adbf5c6eff4146e5a90b3899f
Your Name <you@example.com>
2019-02-17 16:06:15 +0900

    2019.02.17

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 // index.js
-const h = root=>map=>({
+const handle = root=>map=>({
     get: (target, name)=>{
         if(!target[name] && map[name]) 
             target[name] = require(`${root}/${map[name]}`)
         return target[name]
     }
 })
-const f = root=>map=>new Proxy({}, h(root)(map))
-module.exports = f
+
+const req = root=>map=>new Proxy({}, handle(root)(map))
+
+module.exports = req
+
+if(module.parent) return
+
+// sample
+const samp = require('./index.js')('./')({
+    req:  'index.js'
+   ,req2: 'index.js'
+})
+console.log(`** 1: samp:`, samp)
+samp.req
+console.log(`** 2: samp:`, samp)
+samp.req2
+console.log(`** 3: samp:`, samp)
